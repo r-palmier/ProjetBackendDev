8560a39bba0466dd7596de38a22fba25cd662601
rpalmier <rpalmier@et.intechinfo.fr>
2025-03-03 17:26:16 +0100

    DDB

--- ProjectBackendDev/API/views.py
+++ ProjectBackendDev/API/views.py
@@ -6,3 +6,13 @@ from .serializers import CandidateSerializer
 class CandidateListCreate(generics.ListCreateAPIView):
     queryset = Candidate.objects.all()
     serializer_class = CandidateSerializer
+    
+from django.db import connection
+
+def test_db_connection():
+    try:
+        connection.ensure_connection()
+        print("Database connection successful!")
+    except Exception as e:
+        print(f"Database connection failed: {e}")
+
--- ProjectBackendDev/ProjectBackendDev/settings.py
+++ ProjectBackendDev/ProjectBackendDev/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 from environs import Env
 
+
 env = Env()
 env.read_env()
 
@@ -80,6 +81,7 @@ WSGI_APPLICATION = 'ProjectBackendDev.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -88,6 +90,9 @@ DATABASES = {
         'PASSWORD': env.str('DB_PASSWORD'),
         'HOST': env.str('DB_HOST'),
         'PORT': env.int('DB_PORT'),
+        'OPTIONS': {
+            'client_encoding': 'UTF8',
+        },
     }
 }
 
